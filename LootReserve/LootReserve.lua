6711e44f2d4ad92a368369facd76ab00748f728c
ZEUStiger <58010293+ZEUStiger@users.noreply.github.com>
2020-07-15 02:08:53 +0300

    Fixed soulbound tradeable item detection

--- LootReserve/LootReserve.lua
+++ LootReserve/LootReserve.lua
@@ -198,19 +198,19 @@ end
 
 function LootReserve:IsItemSoulboundTradeable(bag, slot)
     if not self.TooltipScanner then
-        self.TooltipScanner = CreateFrame("GameTooltip", "LootReserveTooltipScanner", UIParent);
+        self.TooltipScanner = CreateFrame("GameTooltip", "LootReserveTooltipScanner", UIParent, "GameTooltipTemplate");
         self.TooltipScanner:Hide();
     end
 
     if not self.TooltipScanner.SoulboundTradeable then
-        self.TooltipScanner.SoulboundTradeable = BIND_TRADE_TIME_REMAINING:gsub("%%s", "(.+)");
+        self.TooltipScanner.SoulboundTradeable = BIND_TRADE_TIME_REMAINING:gsub("%.", "%%."):gsub("%%s", "(.+)");
     end
 
     self.TooltipScanner:SetOwner(UIParent, "ANCHOR_NONE");
     self.TooltipScanner:SetBagItem(bag, slot);
     for i = 50, 1, -1 do
         local line = _G[self.TooltipScanner:GetName() .. "TextLeft" .. i];
-        if line and text and text:match(self.TooltipScanner.SoulboundTradeable) then
+        if line and line:GetText() and line:GetText():match(self.TooltipScanner.SoulboundTradeable) then
             self.TooltipScanner:Hide();
             return true;
         end
